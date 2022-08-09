bd0e8ad810ae70e58bb219191b003a53d1396443
Kevin Westphal <westphal@consider-it.de>
2022-08-09 13:04:58 +0200

    feat: helper method to get ReadableMap fields gets fields and falls back to default if field is undefined

---
+++ android/src/main/java/com/rnmqtt/utils/Helpers.kt
@@ -0,0 +1,32 @@
+package com.rnmqtt.utils
+
+import android.util.Log
+import com.facebook.react.bridge.*
+import java.util.*
+
+fun createClientReference(): String = UUID.randomUUID().toString()
+
+@Suppress("UNCHECKED_CAST")
+fun<T> ReadableMap.getOr(field: String, default: T): T {
+  return try {
+    when (this.getType(field)) {
+      ReadableType.Number -> this.getInt(field) as T
+      ReadableType.Array -> this.getArray(field) as T
+      ReadableType.Boolean -> this.getBoolean(field) as T
+      ReadableType.String -> this.getString(field) as T
+      ReadableType.Map -> this.getMap(field) as T
+      ReadableType.Null -> default
+    }
+  } catch (e: Throwable) {
+    Log.e("RnMqtt", "failed to read field of ReadableMap: ", e)
+    default
+  }
+}
+
+fun String.hexToBytes(): ByteArray {
+  check(length % 2 == 0) { "Hex string must have an even length." }
+
+  return chunked(2)
+    .map { it.toInt(16).toByte() }
+    .toByteArray()
+}
