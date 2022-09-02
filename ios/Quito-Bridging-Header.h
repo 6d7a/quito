ad9e3b43eca26ee2f45aaa39370c7c6651ee8101
Kevin Westphal <westphal@consider-it.de>
2022-09-02 14:34:42 +0200

    fix: add cocoa deps to bridging header

--- ios/Quito-Bridging-Header.h
+++ ios/Quito-Bridging-Header.h
@@ -1,3 +1,7 @@
 #import <React/RCTBridgeModule.h>
 #import <React/RCTViewManager.h>
 #import <React/RCTEventEmitter.h>
+
+#import <CocoaMQTT/CocoaMQTT.h>
+#import <CocoaAsyncSocket/GCDAsyncSocket.h>
+#import <CocoaAsyncSocket/GCDAsyncUdpSocket.h>
