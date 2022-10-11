956ef34c608dec9647589cff4abebe6b65bed21a
6d7a <kevinwestphal@alum.calarts.edu>
2022-10-11 15:44:26 +0200

    test: add reset and eventTrigger to emitter mock

--- src/__tests__/__mocks__/NativeEventEmitter.mock.ts
+++ src/__tests__/__mocks__/NativeEventEmitter.mock.ts
@@ -3,6 +3,13 @@ class NativeEventEmitterMock {
   listeners: { event: string; callback: (event: any) => void }[] = [];
 
   constructor(_: any) {}
+
+  resetMock() {
+    this.listeners = []
+  }
+  triggerEvent(eventType: string, params: any) {
+    this.listeners.filter(l => l.event === eventType).forEach(l => l.callback(params))
+  }
   removeAllListeners(eventType: string) {
     this.listeners = this.listeners.filter((l) => l.event !== eventType);
   }
